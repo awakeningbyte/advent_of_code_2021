fc00f433eee04130bd9bc1d94e42a7d14d72912e
George Chen <georgc@iqmetrix.com>
2022-11-14 12:38:48 -0600

    day12 part 1

--- day12/p1.fsx
+++ day12/p1.fsx
@@ -1,4 +1,5 @@
 open System
+open System.Collections.Generic
 let inputs = IO.File.ReadAllLines("test.txt")
 
 let graph =
@@ -14,12 +15,36 @@ let graph =
   |> Seq.groupBy(fun (a,b) -> a)
   |> Map.ofSeq
 
+let visited  =
+  graph
+  |> Seq.map(fun p -> (p.Key, (false, Seq.empty<string>)))
+  |> dict
+
 let rec bfs que =
   match que with
   |[] -> ()
-  | h::remains ->
-    let links = graph[h]
+  | h::tail ->
+
+    let toAdd =
+      graph[h] //linked nodes
+      |> Seq.filter(fun (_, linked) -> // remove visited
+          let (b, _) = visited.[linked]
+          not b
+      ) //filter all visited
+      |> Seq.map(fun (_, linked) ->
+        let (_, tr) = visited.[linked]
+        // let trace = tr
+        let trace1 = seq { yield! tr; h }
+        let mark =
+          match h with
+          | "start" | "end" -> true
+          | x when ("A" <= x) && ("Z" >= x) -> false
+          | z when ("a" <= z) && ("z" >= z) -> true
+          | _ -> failwith "invalid input"
+        visited.[linked] <- (mark, trace1) //mark visit
+        linked
+      )
  
-    bfs remains
+    bfs [ yield! tail ; yield! toAdd ]
 
 bfs ["start"]
