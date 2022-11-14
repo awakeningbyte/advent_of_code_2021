fa7683a62f1492aee012de49fe2489a6c8522e8d
monstercoder <monstercoder@gmail.com>
2022-11-14 11:39:24 -0600

    day12

--- day12/p1.fsx
+++ day12/p1.fsx
@@ -5,15 +5,21 @@ let graph =
   inputs 
   |> Seq.map (fun line -> 
     match line.Split("-") with
-    | [|"start"; b|] -> [| [|"start"; b|]|]
-    | [|a; "end"|] -> [| [|a; "end"|] |]
-    | [|a; b|] -> [| [|a; b|]; [|b;  a|] |]
+    | [|"start"; b|] -> [| ("start", b)|]
+    | [|a; "end"|] -> [| (a, "end") |]
+    | [|a; b|] -> [| (a, b); (b,  a) |]
     | _ -> failwith "invalid inputs"
     )
   |> Seq.collect(fun a -> a)
-  |> Seq.groupBy(fun a ->
-    match a with
-    | [|x;y|] -> x
-    | _ -> failwith "invalid input"
-  )
+  |> Seq.groupBy(fun (a,b) -> a)
   |> Map.ofSeq
+
+let rec bfs que =
+  match que with
+  |[] -> ()
+  | h::remains ->
+    let links = graph[h]
+ 
+    bfs remains
+
+bfs ["start"]
