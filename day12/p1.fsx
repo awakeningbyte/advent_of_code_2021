3b51709a205dcb238a791d7fa8fbd42220f0eee4
George Chen <georgc@iqmetrix.com>
2022-11-14 15:59:49 -0600

    day12 part1

--- day12/p1.fsx
+++ day12/p1.fsx
@@ -21,13 +21,14 @@ let visited  =
   |> dict
   |> Dictionary
 
-visited.Add("end", (true, Seq.empty<string>))
+visited.Add("end", (false, Seq.empty<string>))
 
 let rec bfs que =
   match que with
-  |[] -> ()
+  |[]  -> ()
+  | "end" :: tail -> bfs tail
   | h::tail ->
-
+    // printf "deque %s\n" h
     let toAdd =
       graph[h] //linked nodes
       |> Seq.filter(fun (_, linked) -> // remove visited
@@ -37,20 +38,46 @@ let rec bfs que =
       |> Seq.map(fun (_, linked) ->
         let (_, tr) = visited.[linked]
         // let trace = tr
-        let trace1 = seq { yield! tr; h }
+        let trace1 = seq { yield! tr; h } |> Array.ofSeq
         
-        match h with
+        match linked with
         | "end" -> 
-          visited.[linked] <- (true, trace1) 
+          visited.[linked] <- (false, Set.ofSeq trace1) 
+          // printfn "%s -> %s %A " h "end" visited.[linked]
         | x when ("A" <= x) && ("Z" >= x) -> 
-          visited.[linked] <- (false, trace1) 
+          visited.[linked] <- (false, Set.ofSeq  trace1) 
+          // printfn "%s -> %s %A " h x visited.[linked]
         | z when ("a" <= z) && ("z" >= z) -> 
-          visited.[linked] <- (true, trace1) 
+          visited.[linked] <- (true, Set.ofSeq  trace1) 
+          // printfn "%s -> %s %A " h z visited.[linked]
         | _ -> failwith "invalid input"
         |> ignore
+
         linked
       )
  
     bfs [ yield! tail ; yield! toAdd ]
 
 bfs ["start"]
+
+let rec BackTrace node (upstream: list<string>) =
+  match node with
+  | "start" -> seq { ["start"]}
+  | _ ->
+    let (_, trace) = visited.[node]
+    trace
+    |> Seq.collect(fun x -> 
+        match x with
+        | _ when ("a" <= x) && ("z" >= x) && (List.exists(fun i -> i =x) upstream)->  Seq.empty
+        | _ ->
+          BackTrace x [yield! upstream; x]
+          |> Seq.map(fun y -> 
+            [ yield! y; node ]
+          )
+      )
+ 
+// printfn "%A" visited.["end"]
+BackTrace "end" []
+|> Seq.iter (fun x -> 
+    printfn "%A" x
+  )
